094e6ee4efbc9c63abbcc4624888e4c93c4b3cbb
Anjani Sai Kumar Chatla <anjanichatla92@gmail.com>
2018-02-19 00:49:11 -0700

    Accuracy increased to 81.2

--- cnn_classifier.py
+++ cnn_classifier.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 # For CNN implementation
 import tflearn
-from tflearn.layers.conv import conv_2d, max_pool_2d
+from tflearn.layers.conv import conv_2d, max_pool_2d, global_avg_pool
 from tflearn.layers.core import input_data, dropout, fully_connected
 from tflearn.layers.estimator import regression
 
@@ -17,7 +17,7 @@ import matplotlib.pyplot as plt
 training_dir = './data/train'
 test_dir = './data/test1'
 img_size = 50
-learning_rate = 1e-5
+learning_rate = 1e-4
 
 model = 'CNN Classifier-{}-{}.model'.format(learning_rate, '6conv-basic')
 
@@ -80,26 +80,27 @@ test_data = np.load('test_data.npy')
 tf.reset_default_graph()
 convnet = input_data(shape=[None, img_size, img_size, 1], name='input')
 
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+convnet = conv_2d(convnet, 32, 3, activation='relu')
+convnet = max_pool_2d(convnet, 2)
 
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+convnet = conv_2d(convnet, 64, 3, activation='relu')
+convnet = max_pool_2d(convnet, 2)
 
-convnet = conv_2d(convnet, 128, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+convnet = conv_2d(convnet, 128, 3, activation='relu')
+convnet = max_pool_2d(convnet, 2)
 
-convnet = conv_2d(convnet, 64, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+convnet = conv_2d(convnet, 64, 3, activation='relu')
+convnet = max_pool_2d(convnet, 2)
 
-convnet = conv_2d(convnet, 32, 5, activation='relu')
-convnet = max_pool_2d(convnet, 5)
+convnet = conv_2d(convnet, 32, 3, activation='relu')
+convnet = global_avg_pool(convnet)
+convnet = dropout(convnet, 0.3)
 
-convnet = fully_connected(convnet, 1024, activation='relu')
-convnet = dropout(convnet, 0.8)
+convnet = fully_connected(convnet, 100, activation='relu')
+convnet = dropout(convnet, 0.2)
 
 convnet = fully_connected(convnet, 2, activation='softmax')
-convnet = regression(convnet, optimizer='adam', learning_rate=learning_rate,
+convnet = regression(convnet, optimizer='rmsprop', learning_rate=learning_rate,
                      loss='categorical_crossentropy', name='targets')
 
 model = tflearn.DNN(convnet, tensorboard_dir='log')
@@ -113,7 +114,7 @@ test_x = np.array([i[0] for i in test]).reshape(-1, img_size, img_size, 1)
 test_y = [i[1] for i in test]
 
 
-model.fit({'input': X}, {'targets': Y}, n_epoch=5,
+model.fit({'input': X}, {'targets': Y}, n_epoch=15,
           validation_set=({'input': test_x}, {'targets': test_y}),
           snapshot_step=500, show_metric=True, run_id=str(model))
 model.save(str(model))
